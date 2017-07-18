26ec49c56f8ed24d6f95a0c71fb437b61cea725d
prashanthreddy8143 <persi.143@gmail.com>
2017-07-18 11:44:30 +0530

    my imporetd java

---
+++ Myindex.java
@@ -0,0 +1,32 @@
+this is new file
+/*welcome to git class 25*/
+package com.selenium;
+
+public class Abstract {
+
+	public static void main(String[] args) {
+		A x=new B();
+		System.out.println(x instanceof A);
+		System.out.println(x instanceof B);
+		x.m1();//why it is displaying B class method?
+		x.m2();//why it is displaying C class method?
+
+	}
+
+}
+
+abstract class A{
+	abstract void m1();
+	void m2(){
+		System.out.println("m1() in class C");
+	}
+}
+
+
+
+
+class C extends B{
+	void m1(){
+		System.out.println("m1() in Class B");
+	}
+}
